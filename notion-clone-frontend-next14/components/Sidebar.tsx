5ae471b6361003514746df567e3e9ec2e12c30bf
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-11-26 18:43:41 +0500

    added shadcn-sheet, next14

--- notion-clone-frontend-next14/components/Sidebar.tsx
+++ notion-clone-frontend-next14/components/Sidebar.tsx
@@ -10,24 +10,31 @@ import {
 } from "@/components/ui/sheet";
 
 import NewDocumentButton from "./NewDocumentButton"
+import { FaBars } from "react-icons/fa6";
 
 const Sidebar = () => {
   return (
     <div className="md:w-[12vw] flex p-2 md:py-8 justify-center bg-gray-200 h-full relative">
-      <Sheet>
-        <SheetTrigger>Open</SheetTrigger>
-        <SheetContent side="left">
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
+      {/* Mobile Sidebar - Section */}
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger><FaBars className="w-6 h-6" /></SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
 
-      <NewDocumentButton />
+      {/* Desktop Sidebar - Section */}
+      <div className="hidden md:block">
+        <NewDocumentButton />
+      </div>
     </div>
   );
 }
